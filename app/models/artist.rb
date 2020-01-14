7f41af9970cc97c21ddc6679cea3f446f6112263
github username <github email address>
2020-01-14 17:06:39 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,39 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map(&:gallery)
+  end
+
+  def cities
+    galleries.city
+  end
+
+  def self.total_experience
+    all.select(&:years_experience).sum
+  end
+
+  def self.most_prolific
+    all.max_by { |artist| artist.paintings.length / artist.years_experience }
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, artist, gallery)
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,12 @@
 require_relative '../config/environment.rb'
+# ------------------Artist---------------------#
 
 
+# ------------------Painting---------------------#
+p1 = Painting.new("mona lisa", 40000, )
 
+
+# ------------------Gallery---------------------#
 
 binding.pry
 
