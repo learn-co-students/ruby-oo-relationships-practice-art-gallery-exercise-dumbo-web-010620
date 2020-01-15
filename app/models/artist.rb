d658c96172cc43aa3abf6bec62206061434f0a20
wenglin1 <wenglin@buffalo.edu>
2020-01-15 10:55:34 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,36 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+  @@all = []
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|p| p.artist == self}
+  end
+
+  def galleries
+    paintings.map(&:gallery)
+  end
+
+  def cities
+    galleries.map(&:city)
+  end
+
+  def self.total_experience
+    self.all.map {|x| x.years_experience}.sum
+  end
+
+  def self.most_prolific
+    self.all.map {|x| x.years_experience}.max
+  end
 end
